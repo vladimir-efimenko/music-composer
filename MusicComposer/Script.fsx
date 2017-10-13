c36366afeb619fed2299e8d13f7a54fce8812512
Yefymenko Vladimir <vladimir.efimenko@gmail.com>
2017-10-13 17:38:36 +0300

    Чижик-Пыжик melody within Script

--- MusicComposer/Script.fsx
+++ MusicComposer/Script.fsx
@@ -14,22 +14,24 @@
 
 open MusicComposer
 
-Piece("MUSICC!", ResizeArray([Measure((4,4), 
-                                    [
-                                        NoteFactory.B4.WithDuration Duration.Eighth
-                                        NoteFactory.G4.WithDuration Duration.Eighth
-                                        NoteFactory.B4.WithDuration Duration.Eighth
-                                        NoteFactory.G4.WithDuration Duration.Eighth
-                                        NoteFactory.C5.WithDuration Duration.Eighth
-                                        NoteFactory.B4.WithDuration Duration.Eighth
-                                        NoteFactory.A4.WithDuration Duration.Half
-                                        NoteFactory.D4.WithDuration Duration.Eighth
-                                        NoteFactory.D4.WithDuration Duration.Eighth
-                                        NoteFactory.D4.WithDuration Duration.Eighth
-                                        NoteFactory.D4.WithDuration Duration.Eighth
-                                        NoteFactory.G4.WithDuration Duration.Eighth
-                                        NoteFactory.G4.WithDuration Duration.Eighth
-                                        NoteFactory.G4.WithDuration Duration.Half
-                                    ], Key.GMajor)]))
+let melody = [
+                NoteFactory.B4.WithDuration Duration.Eighth
+                NoteFactory.G4.WithDuration Duration.Eighth
+                NoteFactory.B4.WithDuration Duration.Eighth
+                NoteFactory.G4.WithDuration Duration.Eighth
+                NoteFactory.C5.WithDuration Duration.Eighth
+                NoteFactory.B4.WithDuration Duration.Eighth
+                NoteFactory.A4.WithDuration Duration.Quarter
+                NoteFactory.D4.WithDuration Duration.Eighth
+                NoteFactory.D4.WithDuration Duration.Eighth
+                NoteFactory.D4.WithDuration Duration.Eighth
+                NoteFactory.E4.WithDuration Duration.``16th``
+                NoteFactory.F4Sharp.WithDuration Duration.``16th``
+                NoteFactory.G4.WithDuration Duration.Eighth
+                NoteFactory.G4.WithDuration Duration.Eighth
+                NoteFactory.G4.WithDuration Duration.Quarter
+            ]
+
+Piece("MUSICC!", ResizeArray [Measure((4,4),  melody |> Seq.take 7, Key.GMajor); Measure( (4,4), melody |> Seq.skip 7 |> Seq.take 8, Key.GMajor)] )
 
 |> MusicXMLWriter.write @"C:\Users\YEFVOL\Documents\temp.xml" 
